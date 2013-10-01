136fc9c7ab2847a6dcd45bcf0b1a949027682081
Matthbo <matthijs.booman@hotmail.com>
2013-10-01 15:49:12 +0200

    some changes

--- tmm_common/matthbo/mods/tmm/lib/Reference.java
+++ tmm_common/matthbo/mods/tmm/lib/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 	
 	public static final String MOD_ID = "TMM";
 	public static final String MOD_NAME = "Too Many Mods";
-	public static final String VERSION = "0.00.1";
+	public static final String VERSION = "0.01.1";
 	public static final String CHANNEL = "TMM";
 
 }
