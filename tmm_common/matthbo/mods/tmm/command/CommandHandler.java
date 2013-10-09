01ac2b7926e9c142e18e4d00a4a3828ebe118118
Matthbo <matthijs.booman@hotmail.com>
2013-10-09 21:02:29 +0200

    added tank you message

---
+++ tmm_common/matthbo/mods/tmm/command/CommandHandler.java
@@ -0,0 +1,46 @@
+package matthbo.mods.tmm.command;
+
+import net.minecraft.network.INetworkManager;
+import net.minecraft.network.NetLoginHandler;
+import net.minecraft.network.packet.NetHandler;
+import net.minecraft.network.packet.Packet1Login;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.EnumChatFormatting;
+import cpw.mods.fml.common.network.IConnectionHandler;
+import cpw.mods.fml.common.network.Player;
+
+public class CommandHandler implements IConnectionHandler {
+	
+	public final String TMMMessage = "[TMM] ";
+
+	@Override
+	public void playerLoggedIn(Player player, NetHandler netHandler, INetworkManager manager) {
+		netHandler.getPlayer().addChatMessage(EnumChatFormatting.GRAY + TMMMessage + "You use a development build, Thank you for testing!");
+	}
+
+	@Override
+	public String connectionReceived(NetLoginHandler netHandler, INetworkManager manager) {
+		return null;
+	}
+
+	@Override
+	public void connectionOpened(NetHandler netClientHandler, String server, int port, INetworkManager manager) {
+
+	}
+
+	@Override
+	public void connectionOpened(NetHandler netClientHandler, MinecraftServer server, INetworkManager manager) {
+
+	}
+
+	@Override
+	public void connectionClosed(INetworkManager manager) {
+
+	}
+
+	@Override
+	public void clientLoggedIn(NetHandler clientHandler, INetworkManager manager, Packet1Login login) {
+
+	}
+
+}
--- tmm_common/matthbo/mods/tmm/lib/Reference.java
+++ tmm_common/matthbo/mods/tmm/lib/Reference.java
@@ -6,5 +6,8 @@ public class Reference {
 	public static final String MOD_NAME = "Too Many Mods";
 	public static final String VERSION = "0.01.1";
 	public static final String CHANNEL = "TMM";
+	public static final String DEPENDS = "required-after:Forge@[9.10,)";
+	
+	public static final boolean Devbuild = true;
 
 }
